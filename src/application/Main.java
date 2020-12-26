6a4aaaeaa88c0806ebfbd35560dfdb7062a1b5e3
DeathSpey <leandropontaltdk2012@hotmail.com>
2020-12-26 16:31:45 -0300

    good solution

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
 import model.entites.Resevertion;
+import model.exceptions.DomainException;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -9,24 +10,21 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args)  {
 
         Scanner sc = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            System.out.print("Room number: ");
+            int number = sc.nextInt();
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            Date checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            Date checkOut = sdf.parse(sc.next());
 
-        System.out.print("Room number: ");
-        int number = sc.nextInt();
-        System.out.print("Check-in date (dd/MM/yyyy): ");
-        Date checkIn = sdf.parse(sc.next());
-        System.out.print("Check-out date (dd/MM/yyyy): ");
-        Date checkOut = sdf.parse(sc.next());
 
-        if(!checkOut.after(checkIn)){
-            System.out.println("Error in reservation: Check-out date must be after check-in date");
-        }
-        else {
-            Resevertion resevertion = new Resevertion(number,checkIn,checkOut);
-            System.out.println("Reservention: "+resevertion);
+            Resevertion resevertion = new Resevertion(number, checkIn, checkOut);
+            System.out.println("Reservention: " + resevertion);
 
             System.out.println();
             System.out.println("Enter data to update the reservetion: ");
@@ -36,16 +34,18 @@ public class Main {
             checkOut = sdf.parse(sc.next());
 
 
+            resevertion.updateDates(checkIn, checkOut);
 
-                String error = resevertion.updateDates(checkIn,checkOut);
-                if(error != null) {
-
-                    System.out.println("Error in reservention: " + error);
-                }
-                else{
-                    System.out.println("Reservention: " + resevertion);
-                }
-
+            System.out.println("Reservention: " + resevertion);
+        }
+        catch (ParseException e ){
+            System.out.println("Invalid date format");
+        }
+        catch (DomainException e ){
+            System.out.println("Error in reservetion: "+e.getMessage());
+        }
+        catch (RuntimeException e){
+            System.out.println("Unexpected error");
         }
 
         sc.close();
